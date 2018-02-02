cc4e49324da380a57d7655dd0b9a55f1c63e4559
Kumonda221 <kumonda@foxmail.com>
2018-02-02 17:24:34 +0800

    增量更新 20180202

--- src/org/kucro3/keleton/kernel/klink/kernel/IgnoreModule.java
+++ src/org/kucro3/keleton/kernel/klink/kernel/IgnoreModule.java
@@ -1,4 +1,36 @@
 package org.kucro3.keleton.kernel.klink.kernel;
 
-public class IgnoreModule {
+import com.google.common.eventbus.Subscribe;
+import org.kucro3.keleton.kernel.KeletonKernel;
+import org.kucro3.keleton.module.event.KeletonLoaderEvent;
+import org.kucro3.klink.Ref;
+import org.kucro3.klink.expression.Expression;
+import org.kucro3.klink.expression.ExpressionCompiler;
+import org.kucro3.klink.expression.ExpressionInstance;
+import org.kucro3.klink.expression.ExpressionLibrary;
+import org.kucro3.klink.syntax.Sequence;
+import org.spongepowered.api.event.cause.Cause;
+
+public class IgnoreModule implements ExpressionCompiler.Level1 {
+    @Override
+    public ExpressionInstance compile(ExpressionLibrary expressionLibrary, Ref[] refs, Sequence sequence)
+    {
+        final String name = sequence.next();
+
+        return (sys, env) -> KeletonKernel.getEventBus().register(new Object() {
+            @Subscribe
+            public void onDiscover(KeletonLoaderEvent.Pre event)
+            {
+                if(event.getInfo().id().equals(name))
+                    event.cancel(Cause.source(KeletonKernel.getKlink()).build());
+            }
+        });
+    }
+
+    public static Expression instance()
+    {
+        return INSTANCE;
+    }
+
+    private static final Expression INSTANCE = new Expression("kernel:IgnoreModule", new IgnoreModule());
 }
--- src/org/kucro3/keleton/kernel/klink/kernel/XMounter_kernel.java
+++ src/org/kucro3/keleton/kernel/klink/kernel/XMounter_kernel.java
@@ -1,4 +1,23 @@
 package org.kucro3.keleton.kernel.klink.kernel;
 
-public class XMounter_kernel {
+import org.kucro3.keleton.klink.xmount.Mounter;
+import org.kucro3.klink.Klink;
+import org.kucro3.klink.expression.ExpressionLibrary;
+
+public class XMounter_kernel implements Mounter {
+    @Override
+    public void mount(Klink klink)
+    {
+        ExpressionLibrary lib = klink.getExpressions();
+
+        lib.putExpression(IgnoreModule.instance());
+    }
+
+    @Override
+    public void unmount(Klink klink)
+    {
+        ExpressionLibrary lib = klink.getExpressions();
+
+        lib.removeExpression(IgnoreModule.instance().getName());
+    }
 }
